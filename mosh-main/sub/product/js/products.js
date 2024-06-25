d947954afce27c202d198231d3d16427ce7e590c
hyorim <hyorimseo9808@gmail.com>
2024-06-25 23:53:14 +0900

    hyo

---
+++ mosh-main/sub/product/js/products.js
@@ -0,0 +1,70 @@
+/* 제품 이미지 컬러 변경 */
+document.querySelectorAll('.item_img_box').forEach(product => {
+    const mainImage = product.querySelector('.mainImage');
+    const originalSrc = mainImage.src;
+
+    product.querySelectorAll('.color-option').forEach(option => {
+      option.addEventListener('mouseover', () => {
+        const color = option.getAttribute('data-color');
+        mainImage.src = `images/products_${color}.png`;
+      });
+      option.addEventListener('mouseout', () => {
+        mainImage.src = originalSrc; //원본 이미지로 복원
+      });
+    });
+});
+
+
+/* 필터 토글 */
+$(document).ready(function () {
+    $(".filter_wrap").click(function(){
+        $(".filter_wrap").toggleClass("filter_open");
+    });
+
+    $(".prod_list ul li").click(function(){
+        $(".prod_list ul li").removeClass("selected_menu");
+        $(this).addClass("selected_menu");
+    });
+});
+
+
+/* ajax - 카테고리 */
+function dspFunc(dsp_cont){
+    document.getElementById("prod_menu_page").innerHTML = dsp_cont;
+}
+
+function getFile(url, callbackFunc){
+    const req = new XMLHttpRequest();
+    req.onload = function(){
+        if(req.readyState == 4 && req.status == 200){
+            callbackFunc(this.responseText);
+        } else {
+            callbackFunc("Error : " + req.status);
+        }
+
+        /*페이지 로드 후 스크립트 안먹는 오류 해결*/
+        var script = document.createElement("script"); // 스크립트 요소 생성
+        script.src = "js/products.js"; // 스크립트 src 속성 설정
+        document.body.appendChild(script); // 스크립트 요소를 문서의 body에 추가
+    }
+    req.open("GET", url);
+    req.send();
+}
+
+/* 관심상품 아이콘 */
+$(document).ready(function(){
+    const mtHeart = "images/like.png";
+    const fullHeart = "images/full_like.png";
+
+    $(".heart").click(function(){
+        const img = $(this).find("a img");
+        const currentSrc = img.attr("src");
+        
+        if(currentSrc === mtHeart) {
+            img.attr("src", fullHeart);
+        } else {
+            img.attr("src", mtHeart);
+        }
+        
+    });
+});
--- sub/product/js/products.js
+++ sub/product/js/products.js
@@ -1,25 +1,19 @@
-/* 제품 이미지 컬러 변경 */
+
 document.querySelectorAll('.item_img_box').forEach(product => {
     const mainImage = product.querySelector('.mainImage');
-    const originalSrc = mainImage.src;
 
     product.querySelectorAll('.color-option').forEach(option => {
       option.addEventListener('mouseover', () => {
         const color = option.getAttribute('data-color');
-        mainImage.src = `images/products_${color}.png`;
-      });
-      option.addEventListener('mouseout', () => {
-        mainImage.src = originalSrc; //원본 이미지로 복원
+        mainImage.src = `images/products_tumbler_${color}.png`;
       });
     });
-});
+}); // 제품 이미지 컬러 변경
 
-
-/* 필터 토글 */
 $(document).ready(function () {
     $(".filter_wrap").click(function(){
         $(".filter_wrap").toggleClass("filter_open");
-    });
+    }); //필터 토글
 
     $(".prod_list ul li").click(function(){
         $(".prod_list ul li").removeClass("selected_menu");
@@ -27,44 +21,15 @@ $(document).ready(function () {
     });
 });
 
-
-/* ajax - 카테고리 */
 function dspFunc(dsp_cont){
     document.getElementById("prod_menu_page").innerHTML = dsp_cont;
 }
 
-function getFile(url, callbackFunc){
+function getFile(callbackFunc){
     const req = new XMLHttpRequest();
     req.onload = function(){
-        if(req.readyState == 4 && req.status == 200){
-            callbackFunc(this.responseText);
-        } else {
-            callbackFunc("Error : " + req.status);
-        }
-
-        /*페이지 로드 후 스크립트 안먹는 오류 해결*/
-        var script = document.createElement("script"); // 스크립트 요소 생성
-        script.src = "js/products.js"; // 스크립트 src 속성 설정
-        document.body.appendChild(script); // 스크립트 요소를 문서의 body에 추가
+        callbackFunc(this.responseText);
     }
-    req.open("GET", url);
+    req.open("GET", "sss.html");
     req.send();
 }
-
-/* 관심상품 아이콘 */
-$(document).ready(function(){
-    const mtHeart = "images/like.png";
-    const fullHeart = "images/full_like.png";
-
-    $(".heart").click(function(){
-        const img = $(this).find("a img");
-        const currentSrc = img.attr("src");
-        
-        if(currentSrc === mtHeart) {
-            img.attr("src", fullHeart);
-        } else {
-            img.attr("src", mtHeart);
-        }
-        
-    });
-});
