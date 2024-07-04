8d51ee2561d8c9a07901235ecd6aa9f1a1fd5128
min-00 <rldkatjd3579@naver.com>
2024-07-04 09:52:21 +0900

    0704 수정

--- html/join/join2.js
+++ html/join/join2.js
@@ -177,3 +177,19 @@ function search_add() {
             }
         }).open();
     }
+
+    $(document).ready(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 800) {
+                $('#top-btn').fadeIn();
+            } else {
+                $('#top-btn').fadeOut();
+            }
+        });
+
+        $('#top-btn').click(function () {
+            $('html, body').animate({ scrollTop: 0 }, 800);
+            return false;
+        });
+
+    });
